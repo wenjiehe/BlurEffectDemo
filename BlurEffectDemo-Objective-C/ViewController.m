94433cdf9a45d420d6614cd8155af222b79e9024
heweiqi123 <123610105@qq.com>
2019-07-15 18:30:55 +0800

    优化代码

--- BlurEffectDemo-Objective-C/ViewController.m
+++ BlurEffectDemo-Objective-C/ViewController.m
@@ -22,26 +22,37 @@
     [self.view setNeedsLayout];
     [self.view layoutIfNeeded];
     
-    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.baseView.frame.size.width, 200)];
+    label.text = @"HelveticaNeue-Bold";
+    label.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:30];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor redColor];
     
+    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+
+
+    /// 第一种方式
+    /*
     /// 承载模糊效果的视图
     UIVisualEffectView *visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
     visualEffectView.frame = self.baseView.frame;
     
-    /// UIVibrancyEffect作用是放大和调整UIVisualEffectView内容视图的内容的颜色
-    UIVisualEffectView *vibrancyView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-    vibrancyView.frame = self.baseView.frame;
-    
-    [visualEffectView.contentView addSubview:vibrancyView];
-    
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.baseView.frame.size.width - 40, 200)];
-    label.text = @"HelveticaNeue-Bold";
-    label.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:30];
-    label.textAlignment = NSTextAlignmentCenter;
-    label.textColor = [UIColor whiteColor];
-    [vibrancyView.contentView addSubview:label];
+    [visualEffectView.contentView addSubview:label];
     
     [self.view addSubview:visualEffectView];
+    */
+     
+    /// UIVibrancyEffect作用是放大和调整UIVisualEffectView内容视图的内容的颜色
+    /// 第二种方式
+    UIVibrancyEffect *effect = [UIVibrancyEffect effectForBlurEffect:blurEffect];
+    
+    UIVisualEffectView *vibrancyView = [[UIVisualEffectView alloc] initWithEffect:effect];
+    vibrancyView.frame = self.baseView.frame;
+    
+    [vibrancyView.contentView addSubview:label];
+    
+    [self.view addSubview:vibrancyView];
+    
 }
 
 
